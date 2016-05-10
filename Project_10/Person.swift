9d37236257af42cff42484a4a3737f289e1b7fb5
t3ndai <Dzonga@Macintosh-2.local>
2016-05-10 09:41:40 -0400

    Learning about CollectionViews, and Classes

---
+++ Project_10/Person.swift
@@ -0,0 +1,21 @@
+//
+//  Person.swift
+//  Project_10
+//
+//  Created by Tendai Prince Dzonga on 4/19/16.
+//  Copyright © 2016 Tendai Prince Dzonga. All rights reserved.
+//
+
+import UIKit
+
+class Person: NSObject {
+    
+    var name: String
+    var image: String
+
+    init(name: String, image: String){
+        self.name = name
+        self.image = image
+    }
+    
+}
